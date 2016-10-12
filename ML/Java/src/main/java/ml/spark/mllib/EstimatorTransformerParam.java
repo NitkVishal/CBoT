47c7ad7a99b4d63833aa08f53331dc8795172ee4
vishalbabu01 <vishalbabu.in@gmail.com>
2016-10-12 16:57:30 +0530

    TransformationEstimator Program

---
+++ ML/Java/src/main/java/ml/spark/mllib/EstimatorTransformerParam.java
@@ -0,0 +1,5 @@
+package ml.spark.mllib;
+
+public class EstimatorTransformerParam {
+
+}
--- ML/Java/src/main/java/ml/spark/streaming/HelloClass.java
+++
@@ -1,7 +0,0 @@
-package ml.spark.streaming;
-
-public class HelloClass {
-	public static void main (String args[]){
-		System.out.println("Hello CBot");
-	}
-}
